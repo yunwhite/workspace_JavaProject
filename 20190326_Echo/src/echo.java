a0b704e2e2f2c2edac96fce8913be3b4a74cb5ae
yunwhite <s2018s12@e-mirim.hs.kr>
2019-03-26 19:54:33 +0900

    .metadata

--- 20190326_Echo/src/echo.java
+++ 20190326_Echo/src/echo.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
 
 public class echo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		String input;
+		while(true) {
+			System.out.print("아무말이나 입력하세요(exit 입력시 종료) : ");
+			input = sc.nextLine();
+			if (input.equals("exit")) {
+				System.out.println("끝");
+				System.exit(0);
+			}
+			else
+				System.out.println(input);
+		}
 	}
 
 }
