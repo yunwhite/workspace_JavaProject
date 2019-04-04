abffbd470d5c8cbe0200671d9d536d9b127bb239
yunwhite <s2018s12@e-mirim.hs.kr>
2019-04-04 18:26:40 +0900

    add Echo Project

---
+++ 20190402_Major/src/major.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class major {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String major[] = {"인터랙티브미디어과", "뉴미디어소프트웨어과", "뉴미디어솔루션과", "뉴미디어디자인과"}
+		System.out.print("학번을 입력하세요 : ");
+		int n = sc.nextInt();
+		
+		break;
+		}
+	}
+
+}
