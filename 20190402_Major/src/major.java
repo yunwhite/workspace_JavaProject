60d21f00cfb1cd24aca74f1ca432ae351f33a7c6
yunwhite <s2018s12@e-mirim.hs.kr>
2019-04-04 19:24:32 +0900

    add Echo Project

--- 20190402_Major/src/major.java
+++ 20190402_Major/src/major.java
@@ -5,12 +5,11 @@ public class major {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		String major[] = {"인터랙티브미디어과", "뉴미디어소프트웨어과", "뉴미디어솔루션과", "뉴미디어디자인과"}
+		String major[] = {"인터랙티브미디어과", "뉴미디어소프트웨어과", "뉴미디어솔루션과", "뉴미디어디자인과"};
 		System.out.print("학번을 입력하세요 : ");
-		int n = sc.nextInt();
-		
-		break;
-		}
+		String n = sc.next();
+		String grade = n.substring(0, 1);
+		String classn = n.substring(1, 2);
+		System.out.println(major[Integer.parseInt(classn) / 2]);
 	}
-
 }
